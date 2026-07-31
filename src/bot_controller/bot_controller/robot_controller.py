412299a7fb3020bc936d864b8d532d0b75ec5147
Smit <184806981+Masqiller@users.noreply.github.com>
2026-07-31 21:08:44 +0530

    Update robot_controller.py

--- src/bot_controller/bot_controller/robot_controller.py
+++ src/bot_controller/bot_controller/robot_controller.py
@@ -1,23 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import Twist
 
 class RobotController(Node):
     def __init__(self):
         super().__init__('robot_controller')
-        self.publisher_ = self.create_publisher(TwistStamped, '/diff_cont/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, '/diff_cont/cmd_vel', 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.get_logger().info('Robot Controller has been started. Moving bot forward...')
 
     def timer_callback(self):
-        msg = TwistStamped()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = 'base_footprint'
+        msg = Twist()
         # Move forward
-        msg.twist.linear.x = 0.5
+        msg.linear.x = 0.5
         # Add slight rotation just to see it clearly
-        msg.twist.angular.z = 0.2
+        msg.angular.z = 0.2
         self.publisher_.publish(msg)
 
 def main(args=None):
